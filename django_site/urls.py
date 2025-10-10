993eaea4321d555796553378ed2cd5992a17e5be
arthuryoung <arthur22444@gmail.com>
2025-10-10 01:30:58 +0100

    Edited URL to bring sermons to the root

--- django_site/urls.py
+++ django_site/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("sermons/", include("sermons.urls")),
+    path("", include("sermons.urls")),
 ]
